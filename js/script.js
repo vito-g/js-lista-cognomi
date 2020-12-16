fc7918a359519c8d6321526820f7e69f484b4791
Vito Gentile <gentilevito1076@gmail.com>
2020-12-16 16:39:53 +0100

    Cognome Utente: Posizione in Lista Stampata in HTML - FINE ESERCIZIO!

--- js/script.js
+++ js/script.js
@@ -7,17 +7,17 @@ var listaAggiornata = document.getElementById("output");
 //Inserimento (Stampa) risultato (lista aggiornata e Ordinata Alfabeticamente(a mezzo 'sort')) nel Div HTML richiamato:
 listaAggiornata.innerHTML = lista.sort();
 var aggiornamento = lista.sort();
-
-
-// console.log('lista aggiornata: ' + lista.push('surname'));
 var inOrdine = document.getElementById('ordine');
+
 for ( var i=0; i < aggiornamento.length; i++ ) {
-  //Crea un Item per ogni voce della lista (precedentemente aggiornata ed ordinata) e lo inserisce all'interno dell Ul richiamata dalla var inOrdine dichiarata appena sopra
+  //Crea un Item per ogni voce della lista (precedentemente aggiornata ed ordinata) e lo inserisce all'interno dell Ul richiamata dalla var inOrdine dichiarata appena sopra:
   inOrdine.innerHTML += '<li>' + aggiornamento[i] + '</li>';
 }
-// assegnata una var che indica la posizione del cognome utente all'interno dell'array (precedentemente aggiornato e ordinato)
+// assegnata una var che indica l'indice del cognome utente all'interno dell'array (precedentemente aggiornato e ordinato):
 var posizioneArray = aggiornamento.indexOf(surname);
-console.log(surname + ' occupa la posizione numero ' + posizioneArray + ' all\'interno dell\'array.' );
+console.log(surname + ' ha indice ' + posizioneArray + ' all\'interno dell\'array.' );
 // assegnata una var che indica la posizione del cognome utente all'interno della lista (precedentemente aggiornata e ordinata)
 var posizione = posizioneArray + 1;
 console.log(surname + ' occupa la posizione numero ' + posizione + ' all\'interno della lista.' );
+var outputPosition = document.getElementById('position');
+outputPosition.innerText = 'Il cognome utente occupa la posizione numero ' + posizione;
