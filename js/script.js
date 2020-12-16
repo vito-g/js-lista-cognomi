44cf897e45a7c1d96135512a033d9b539026095d
Vito Gentile <gentilevito1076@gmail.com>
2020-12-16 16:28:02 +0100

    Calcolo in Console della posizione Cognome utente in Array e Lista

--- js/script.js
+++ js/script.js
@@ -15,3 +15,9 @@ for ( var i=0; i < aggiornamento.length; i++ ) {
   //Crea un Item per ogni voce della lista (precedentemente aggiornata ed ordinata) e lo inserisce all'interno dell Ul richiamata dalla var inOrdine dichiarata appena sopra
   inOrdine.innerHTML += '<li>' + aggiornamento[i] + '</li>';
 }
+// assegnata una var che indica la posizione del cognome utente all'interno dell'array (precedentemente aggiornato e ordinato)
+var posizioneArray = aggiornamento.indexOf(surname);
+console.log(surname + ' occupa la posizione numero ' + posizioneArray + ' all\'interno dell\'array.' );
+// assegnata una var che indica la posizione del cognome utente all'interno della lista (precedentemente aggiornata e ordinata)
+var posizione = posizioneArray + 1;
+console.log(surname + ' occupa la posizione numero ' + posizione + ' all\'interno della lista.' );
