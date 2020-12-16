521d1946c886e62ed3c166e8a9fb9e3fe012758c
Vito Gentile <gentilevito1076@gmail.com>
2020-12-16 16:14:09 +0100

    Commenti

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var surname = prompt('Inserisci il tuo cognome.');
 var lista = ['Aranzulla', 'Dionigi', 'Conte', 'Casamassima', 'Carone', 'Decandia', 'Saponaro', 'Zaccaria', 'Tramutoli'];
 //Istruzione per Aggiunta in Lista del Cognome Utente:
 lista.push(surname);
-//Richiamo Div HTML:
+//Richiamo Div HTML attraverso var ad esso assegnata:
 var listaAggiornata = document.getElementById("output");
 //Inserimento (Stampa) risultato (lista aggiornata e Ordinata Alfabeticamente(a mezzo 'sort')) nel Div HTML richiamato:
 listaAggiornata.innerHTML = lista.sort();
@@ -12,5 +12,6 @@ var aggiornamento = lista.sort();
 // console.log('lista aggiornata: ' + lista.push('surname'));
 var inOrdine = document.getElementById('ordine');
 for ( var i=0; i < aggiornamento.length; i++ ) {
-  inOrdine.innerHTML += '<li>' + aggiornamento[i] + '</li>'; 
+  //Crea un Item per ogni voce della lista (precedentemente aggiornata ed ordinata) e lo inserisce all'interno dell Ul richiamata dalla var inOrdine dichiarata appena sopra
+  inOrdine.innerHTML += '<li>' + aggiornamento[i] + '</li>';
 }
